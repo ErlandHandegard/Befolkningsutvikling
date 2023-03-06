4fa7e0c532ccf54df761a7ada744291a6bcae1bc
Erland <erland.handegard@outlook.com>
2023-03-06 21:09:36 +0100

    Lagt til fil og fikset encoding

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 fileName = "tabell_bak_i_tid.csv"
 fileNamefuture = "tabell_frem_i_tid.csv"
 
-data = pd.read_csv(filepath_or_buffer="tabell_bak_i_tid.csv", encoding="utf-8")
+data = pd.read_csv(filepath_or_buffer="tabell_bak_i_tid.csv", encoding="ISO-8859-1")
 print(data)
