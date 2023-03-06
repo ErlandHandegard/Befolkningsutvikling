360c10f1397dffb2832881b89b01801fa3689d9e
Erland <erland.handegard@outlook.com>
2023-03-06 21:03:06 +0100

    Slettet fil for å legge til en fikset fil

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 
 fileName = "tabell_bak_i_tid.csv"
 fileNamefuture = "tabell_frem_i_tid.csv"
 
-with open(fileName, encoding = "utf-8-sig") as file:
-    fileContent = csv.reader(file, delimiter=";")
-    
-    while count > 2022-1846:
-        count =+ 1
-
-
-    desiredRowIndex = 3
-    for i in range(desiredRowIndex):
-        next(fileContent)
-
-    desiredRow = next(fileContent)
-    output = desiredRow[2]
-    print(output)
+data = pd.read_csv(filepath_or_buffer="tabell_bak_i_tid.csv", encoding="utf-8")
+print(data)
